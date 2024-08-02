ae30a69d9ec08b1b2981f177a361facfb4fe5d07
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2024-08-02 15:13:57 +0900

    Update bedrock_modelId.py

--- bedrock_modelId.py
+++ bedrock_modelId.py
@@ -3,3 +3,5 @@ import pprint
 
 bedrock = boto3.client("bedrock", region_name="us-east-1")
 pprint.pprint(bedrock.list_foundation_models()["modelSummaries"])
+
+pprint.pprint(bedrock.get_foundartion_model(modelIdentifier = "amazon.titan-text-express-v1"))
